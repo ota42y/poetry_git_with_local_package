1f808bf1ed850db7a4416a10423a37719fe8342d
ota42y <ota42y@gmail.com>
2021-04-02 15:23:44 +0900

    t

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+setup(
+    name="poetry_git_with_local_package",
+    version="0.1.0",
+    install_requires=[
+        'subdir_package @ file://localhost/%s/subdir_package/' % os.getcwd().replace('\\', '/'),
+    ]
+)
