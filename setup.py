191b772ec91a0940acd781e431d7c4bda6eec16e
ota42y <ota42y@gmail.com>
2021-04-05 12:36:41 +0900

    fix file path

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name="poetry_git_with_local_package",
     version="0.1.0",
     install_requires=[
-        'subdir_package @ file://localhost/%s/subdir_package/' % os.getcwd().replace('\\', '/'),
+        'subdir_package @ file:///localhost/%s/subdir_package/' % os.getcwd().replace('\\', '/'),
     ]
 )
